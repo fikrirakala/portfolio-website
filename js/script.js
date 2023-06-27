06063684ee6f81137e860d6c4ab1e1dc4c91ff10
Fikri Rakala <fikrirakala@gmail.com>
2023-06-28 05:37:10 +0800

    create text animation effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-// Add custom JavaScript here
+// Typing effect animation
+const textElement = document.querySelector("#typing-text");
+const text = textElement.textContent;
+
+textElement.textContent = "";
+
+function typingEffect() {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      textElement.textContent += text.charAt(i);
+    }, 100 * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typingEffect);
